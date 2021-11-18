0eda63a4facb7373e9ce52fee6ef77577b38d2bb
jackXu <xujinzhang@372163.com>
2021-11-18 17:44:30 +0800

    webhooks fixed

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"webhook.com/route"
 )
@@ -10,6 +16,34 @@ func main() {
 	var port string //web端口
 	flag.StringVar(&port, "p", "8080", "请输入端口号")
 	flag.Parse() //解析参数
-	r := route.InitRouter()
-	r.Run(":" + port)
+	router := route.InitRouter()
+
+	// 服务初始化
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	// 启动服务
+	go server.ListenAndServe()
+
+	// 平滑重启
+	listenSignal(context.Background(), server)
+}
+
+func listenSignal(ctx context.Context, httpSrv *http.Server) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	<-sigs
+	log.Println("notify sigs")
+	httpSrv.Shutdown(ctx)
+	log.Println("http shudown")
+
+	// select {
+	// case <-sigs:
+	// 	log.Println("notify sigs")
+	// 	httpSrv.Shutdown(ctx)
+	// 	log.Println("http shudown")
+	// }
 }
