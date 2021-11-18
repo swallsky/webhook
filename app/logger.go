c09d15b759d4d9f1ee32f7104f0e1ee4e3bc687e
jackXu <xujinzhang@372163.com>
2021-11-18 13:39:48 +0800

    logger fixed

--- app/logger.go
+++ app/logger.go
@@ -87,23 +87,3 @@ func LoggerToFile() gin.HandlerFunc {
 			reqUri)
 	}
 }
-
-// 日志测试
-func LogTest(c *gin.Context) {
-	// Info 级别日志
-	Logger().WithFields(logrus.Fields{
-		"name": "Info Test",
-	}).Info("记录一下日志", "Info")
-	// Error级别日志
-	Logger().WithFields(logrus.Fields{
-		"name": "Error Test",
-	}).Error("记录错误日志", "Error")
-	// Warn级别日志
-	Logger().WithFields(logrus.Fields{
-		"name": "警告",
-	}).Warn("记录警告日志", "Warn")
-	// Debug级别日志
-	Logger().WithFields(logrus.Fields{
-		"name": "Debug Test",
-	}).Debug("记录Debug日志", "Debug")
-}
