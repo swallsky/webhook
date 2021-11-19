63659c51b3bb68b358ac9178e1e3c2be9a4ede61
jackXu <xujinzhang@372163.com>
2021-11-19 09:40:07 +0800

    restart add

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"webhook.com/route"
 )
@@ -28,22 +29,19 @@ func main() {
 	go server.ListenAndServe()
 
 	// 平滑重启
-	listenSignal(context.Background(), server)
+	listenSignal(server)
 }
 
-func listenSignal(ctx context.Context, httpSrv *http.Server) {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+func listenSignal(httpSrv *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-quit
+	log.Println("Shutdown Server ...")
 
-	<-sigs
-	log.Println("notify sigs")
-	httpSrv.Shutdown(ctx)
-	log.Println("http shudown")
-
-	// select {
-	// case <-sigs:
-	// 	log.Println("notify sigs")
-	// 	httpSrv.Shutdown(ctx)
-	// 	log.Println("http shudown")
-	// }
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := httpSrv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shundown:", err)
+	}
+	log.Println("Server exiting")
 }
